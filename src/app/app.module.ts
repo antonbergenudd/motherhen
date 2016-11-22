4737ff27ebbf17c529996e2570f16d7b65a0ce40
Anton <antonbergenudd@hotmail.com>
2016-11-22 21:11:32 +0100

    AngularFire initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyAelHUeSJwRAxVXuggCM7ZgUu3F3wo1Odg",
+  authDomain: "motherhen-test.firebaseapp.com",
+  databaseURL: "https://motherhen-test.firebaseio.com",
+  storageBucket: "",
+  messagingSenderId: "199003432833"
+};
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
